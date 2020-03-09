c86a5a144431567091e7a2fe97a645db94774246
Freeze88 <andrevitorino1997@hotmail.com>
2020-03-09 15:36:28 +0000

    Fix AI putting pieces on full columns

--- ZetaAI.cs
+++ ZetaAI.cs
@@ -9,10 +9,9 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
     {
         private int maxDepth;
 
-        private int cycles;
-
         public override FutureMove Think(Board board, CancellationToken ct)
         {
+            maxDepth = 6;
             FutureMove mov = (NegaMax(board, 0, float.NegativeInfinity, float.PositiveInfinity, ct).move);
 
             return mov;
@@ -24,44 +23,49 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
 
             if (board.CheckWinner() != Winner.None)
             {
-                if ((board.CheckWinner() == Winner.White && board.Turn == PColor.White) || (board.CheckWinner() == Winner.Red && board.Turn == PColor.Red))
+                if (board.CheckWinner() == board.Turn.ToWinner())
                 {
-                    return (new FutureMove() , float.PositiveInfinity);
+                    return (FutureMove.NoMove, float.PositiveInfinity);
                 }
-                else if ((board.CheckWinner() == Winner.White && board.Turn.Other() == PColor.White) || (board.CheckWinner() == Winner.Red && board.Turn.Other() == PColor.Red))
+                else if (board.CheckWinner() == board.Turn.Other().ToWinner())
                 {
-                    return (new FutureMove() , float.NegativeInfinity);
+                    return (FutureMove.NoMove, float.NegativeInfinity);
                 }
                 else if (board.CheckWinner() == Winner.Draw)
                 {
-                    return (new FutureMove(), 2);
+                    return (FutureMove.NoMove, 0);
                 }
-                else return (new FutureMove(), 0);
+                else return (FutureMove.NoMove, 0);
+            }
+            else if(depth > maxDepth)
+            {
+                return (FutureMove.NoMove, float.NegativeInfinity);
             }
             else
             {
-                (FutureMove move, float value) bestMove = (new FutureMove(0, PShape.Square) , float.NegativeInfinity);
+                (FutureMove move, float value) bestMove = (FutureMove.NoMove, float.NegativeInfinity);
 
                 for (int i = 0; i < board.cols; i++)
                 {
-                    for (int b = 0; b < 2; b++)
+                    if (!board.IsColumnFull(i))
                     {
-                        FutureMove pos;
-                        pos = b == 0? new FutureMove(i, PShape.Round) : new FutureMove(i, PShape.Square);
-
-                        if (pos.shape == PShape.Square)
-                            if (board.squarePieces == 0)
-                                pos = new FutureMove(i,PShape.Round);
-
-                        if (pos.shape == PShape.Round)
-                            if (board.roundPieces == 0)
-                                pos = new FutureMove(i, PShape.Square);
-
-                        if (board.PieceCount(board.Turn, PShape.Round) == 0 || board.PieceCount(board.Turn, PShape.Square) == 0)
-                            return (FutureMove.NoMove, 0);
-
-                        if (!board.IsColumnFull(i) && depth < 6)
+                        for (int b = 0; b < 2; b++)
                         {
+                            //Console.WriteLine(b);
+                            FutureMove pos;
+                            pos = b == 0 ? new FutureMove(i, PShape.Round) : new FutureMove(i, PShape.Square);
+
+                            if (pos.shape == PShape.Square)
+                                if (board.squarePieces == 0)
+                                    pos = new FutureMove(i, PShape.Round);
+
+                            if (pos.shape == PShape.Round)
+                                if (board.roundPieces == 0)
+                                    pos = new FutureMove(i, PShape.Square);
+
+                            if (board.PieceCount(board.Turn, PShape.Round) == 0 || board.PieceCount(board.Turn, PShape.Square) == 0)
+                                return (FutureMove.NoMove, 0);
+
                             float score;
 
                             board.DoMove(pos.shape, pos.column);
@@ -70,7 +74,7 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
 
                             board.UndoMove();
 
-                            if (score > bestMove.value)
+                            if (score >= bestMove.value)
                             {
                                 alpha = score;
 
@@ -83,10 +87,13 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                                 }
                             }
                         }
+
                     }
                 }
+
                 return bestMove;
             }
         }
     }
 }
+
