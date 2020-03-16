f790bd269792fb83b49bc3eb3c1c5efbca2ef6ce
Freeze88 <andrevitorino1997@hotmail.com>
2020-03-16 23:27:44 +0000

    Add new heuristic, improve NegaMax method

--- ZetaAI.cs
+++ ZetaAI.cs
@@ -7,66 +7,57 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
     {
         private int maxDepth;
         private float pieceValue = 5;
-        private float extraPieceValue = 1;
-        private PColor player;
+
         public override FutureMove Think(Board board, CancellationToken ct)
         {
-            player = board.Turn;
-            maxDepth = 2;
-            (FutureMove, float) mov = (NegaMax(board, 0, float.NegativeInfinity, float.PositiveInfinity, ct));
+            maxDepth = 3;
+            (FutureMove, float) mov = (NegaMax(board, board.Turn, 0, float.NegativeInfinity, float.PositiveInfinity, ct));
             Console.WriteLine(mov.Item2);
             return mov.Item1;
         }
 
-        private (FutureMove move, float value) NegaMax(Board board, int depth, float alpha, float beta, CancellationToken ct)
+        private (FutureMove move, float value) NegaMax(Board board, PColor turn, int depth, float alpha, float beta, CancellationToken ct)
         {
-           
             if (board.CheckWinner() != Winner.None)
             {
-                if (board.CheckWinner() == board.Turn.ToWinner())
+                if (board.CheckWinner() == turn.ToWinner())
                 {
                     return (FutureMove.NoMove, float.PositiveInfinity);
                 }
-                else if (board.CheckWinner() == board.Turn.Other().ToWinner())
+                else if (board.CheckWinner() == turn.Other().ToWinner())
                 {
                     return (FutureMove.NoMove, float.NegativeInfinity);
                 }
                 else
                     return (FutureMove.NoMove, 0);
             }
-            else if (depth >= maxDepth)
+            else if (depth == maxDepth)
             {
-                    float v = heuristicValue(board);
-                    //Console.WriteLine("Turn : " + board.Turn.ToString() + "   :   " + (board.Turn == player ? v : -v));
-                    return (FutureMove.NoMove, (board.Turn == player ? v : -v));
+                float v = HeuristicValue(board, turn);
+                return (FutureMove.NoMove, v);
             }
             else
             {
                 (FutureMove move, float value) bestMove = (FutureMove.NoMove, float.NegativeInfinity);
 
-                for (int b = 0; b < 2; b++)
+
+                for (int i = 0; i < board.cols; i++)
                 {
-                    for (int i = 0; i < board.cols - 1; i++)
+                    for (int b = 0; b < 2; b++)
                     {
                         if (!board.IsColumnFull(i))
                         {
-                            FutureMove pos;
+                            FutureMove pos = new FutureMove(i, (PShape)b);
 
-                            pos = (b == 0 ? new FutureMove(i, PShape.Round) : new FutureMove(i, PShape.Square));
-
-                            //Console.WriteLine(pos.shape);
-
-                            if (board.PieceCount(board.Turn, pos.shape) == 0)
+                            if (board.PieceCount(turn, pos.shape) == 0)
                                 pos = pos.shape == PShape.Square ? new FutureMove(i, PShape.Round) : new FutureMove(i, PShape.Square);
 
-                            if (board.PieceCount(board.Turn, PShape.Round) == 0 && board.PieceCount(board.Turn, PShape.Square) == 0)
-                                break;
-
-                            float score;
+                            if (board.PieceCount(turn, PShape.Round) == 0 && board.PieceCount(turn, PShape.Square) == 0)
+                                return bestMove;
 
                             board.DoMove(pos.shape, pos.column);
 
-                            score = -NegaMax(board.Copy(), depth +1, -beta, -alpha, ct).value;
+                            float score = -NegaMax(board, turn.Other(), depth + 1, -beta, -alpha, ct).value;
 
                             board.UndoMove();
 
@@ -74,10 +65,10 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                             {
                                 alpha = score;
 
-                                bestMove.value = score;
-                                bestMove.move = pos;
+                                bestMove = (pos, score);
 
-                                if (alpha >= beta) return bestMove;
+                                if (alpha >= beta)
+                                    return bestMove;
                             }
                         }
                     }
@@ -85,65 +76,147 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                 return bestMove;
             }
         }
-
-        private float heuristicValue(Board board)
+        private float HeuristicValue(Board board, PColor turn)
         {
             float boardValue = pieceValue;
-            for (int j = 0; j < board.cols -1; j++)
+
+            for (int j = 0; j < board.cols; j++)
             {
                 for (int f = 0; f < board.rows; f++)
                 {
                     if (!board[f, j].HasValue)
                     {
-                        if (board[Math.Max(0, f - 1), j].HasValue)
+                        if (f - 1 >= 0 &&  j - 1 >= 0 && j + 1 <= board.cols - 1)
                         {
-                            if (board[Math.Max(0, f - 1), j].Value.shape == board.Turn.Shape()
-                                || board[Math.Max(0, f - 1), j].Value.color == board.Turn)
+                            if (board[f - 1, j - 1].HasValue && board[f - 1, j + 1].HasValue)
                             {
-                                boardValue += 2;
+                                if ((board[f - 1, j - 1].Value.shape == turn.Shape() || board[f - 1, j - 1].Value.color == turn) &&
+                                    (board[f - 1, j + 1].Value.shape == turn.Shape() || board[f - 1, j + 1].Value.color == turn))
+                                {
+                                    boardValue += 10;
+                                }
+                                if ((board[f - 1, j - 1].Value.shape == turn.Other().Shape() || board[f - 1, j - 1].Value.color == turn.Other()) &&
+                                    (board[f - 1, j + 1].Value.shape == turn.Other().Shape() || board[f - 1, j + 1].Value.color == turn.Other()))
+                                {
+                                    boardValue -= 10;
+                                }
                             }
-                            else
-                                boardValue -= 1;
+                            else if (board[f - 1, j - 1].HasValue)
+                            {
+                                if (board[f - 1, j - 1].Value.shape == turn.Shape()
+                                    || board[f - 1, j - 1].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 1, j - 1].Value.shape == turn.Other().Shape()
+                                    || board[f - 1, j - 1].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
+                            }
+                            else if (board[f - 1, j + 1].HasValue)
+                            {
+                                if (board[f - 1, j + 1].Value.shape == turn.Shape()
+                                    || board[f - 1, j + 1].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 1, j + 1].Value.shape == turn.Other().Shape()
+                                    || board[f - 1, j + 1].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
+                            }
                         }
-                        if (board[Math.Max(0, f - 1), Math.Max(0, j - 1)].HasValue)
+
+
+
+                        if (f - 2 >= 0 && j - 1 >= 0 && j + 1 <= board.cols - 1)
                         {
-                            if (board[Math.Max(0, f - 1), Math.Max(0, j - 1)].Value.shape == board.Turn.Shape()
-                                || board[Math.Max(0, f - 1), Math.Max(0, j - 1)].Value.color == board.Turn)
+                            if (board[f - 2, j - 1].HasValue && board[f - 2, j + 1].HasValue)
                             {
-                                boardValue += 1;
+                                if ((board[f - 2, j + 1].Value.shape == turn.Shape() || board[f - 2, j + 1].Value.color == turn) &&
+                                    (board[f - 2, j - 1].Value.shape == turn.Shape() || board[f - 2, j - 1].Value.color == turn))
+                                {
+                                    boardValue += 10;
+                                }
+                                if ((board[f - 2, j + 1].Value.shape == turn.Other().Shape() || board[f - 2, j + 1].Value.color == turn.Other()) &&
+                                    (board[f - 2, j - 1].Value.shape == turn.Other().Shape() || board[f - 2, j - 1].Value.color == turn.Other()))
+                                {
+                                    boardValue -= 10;
+                                }
                             }
-                            else
-                                boardValue -= 2;
+                            else if (board[Math.Max(0, f - 1), Math.Min(board.cols - 1, j + 1)].HasValue)
+                            {
+                                if (board[f - 1, j + 1].Value.shape == turn.Shape()
+                                    || board[f - 1, j + 1].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 1, j + 1].Value.shape == turn.Other().Shape()
+                                    || board[f - 1, j + 1].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
+                            }
+                            else if (board[Math.Max(0, f - 2), Math.Max(0, j - 1)].HasValue)
+                            {
+                                if (board[f - 2, j - 1].Value.shape == turn.Shape()
+                                    || board[f - 2, j - 1].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 2, j - 1].Value.shape == turn.Other().Shape()
+                                    || board[f - 2, j - 1].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
+                            }
                         }
-                        if (board[Math.Max(0, f - 1), Math.Min(board.cols, j + 1)].HasValue)
+
+
+
+                        if (f - 2 >= 0 && f - 3 >= 0)
                         {
-                            if (board[Math.Max(0, f - 1), Math.Min(board.cols, j + 1)].Value.shape == board.Turn.Shape()
-                                || board[Math.Max(0, f - 1), Math.Min(board.cols, j + 1)].Value.color == board.Turn)
+                            if (board[f - 2, j].HasValue && board[f - 3, j].HasValue)
                             {
-                                boardValue += 1;
+                                if ((board[f - 2, j].Value.shape == turn.Shape() || board[f - 2, j].Value.color == turn)
+                                    && (board[f - 3, j].Value.shape == turn.Shape() || board[f - 3, j].Value.color == turn))
+                                {
+                                    boardValue += 10;
+                                }
+                                if ((board[f - 2, j].Value.shape == turn.Other().Shape() || board[f - 2, j].Value.color == turn.Other())
+                                    && (board[f - 3, j].Value.shape == turn.Other().Shape() || board[f - 3, j].Value.color == turn.Other()))
+                                {
+                                    boardValue -= 10;
+                                }
                             }
-                            else
-                                boardValue -= 2;
-                        }
-                        if (board[Math.Max(0, f - 2), Math.Max(0, j - 1)].HasValue)
-                        {
-                            if (board[Math.Max(0, f - 2), Math.Max(0, j - 1)].Value.shape == board.Turn.Shape()
-                                || board[Math.Max(0, f - 2), Math.Max(0, j - 1)].Value.color == board.Turn)
+                            else if (board[f - 2, j].HasValue)
                             {
-                                boardValue += 1;
+                                if (board[f - 2, j].Value.shape == turn.Shape()
+                                    || board[f - 2, j].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 2, j].Value.shape == turn.Other().Shape()
+                                    || board[f - 2, j].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
                             }
-                            else
-                                boardValue -= 2;
-                        }
-                        if (board[Math.Max(0, f - 2), Math.Max(0, j + 1)].HasValue)
-                        {
-                            if (board[Math.Max(0, f - 2), Math.Min(board.cols, j + 1)].Value.shape == board.Turn.Shape()
-                                || board[Math.Max(0, f - 2), Math.Min(board.cols, j + 1)].Value.color == board.Turn)
+                            else if (board[f - 3,j].HasValue)
                             {
-                                boardValue += 1;
+                                if (board[f - 3, j].Value.shape == turn.Shape()
+                                    || board[f - 3, j].Value.color == turn)
+                                {
+                                    boardValue += 2;
+                                }
+                                if (board[f - 3, j].Value.shape == turn.Other().Shape()
+                                    || board[f - 3, j].Value.color == turn.Other())
+                                {
+                                    boardValue -= 2;
+                                }
                             }
-                            else
-                                boardValue -= 2;
                         }
                     }
                 }
