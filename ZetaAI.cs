de64d5904e203224765cc0d3eef9e35e53764cbd
Freeze88 <andrevitorino1997@hotmail.com>
2020-03-24 19:04:49 +0000

    Add comments, small NegaMax improvements

--- ZetaAI.cs
+++ ZetaAI.cs
@@ -5,66 +5,121 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
 {
     internal class ZetaAI : AbstractThinker
     {
-        private int maxDepth = 4;
-        private float winScore = 100000;
+        private readonly float winScore = 100000;
+        private int maxDepth;
 
-        public override FutureMove Think(Board board, CancellationToken ct) => 
-            NegaMax(board.Copy(), board.Turn, 0, float.NegativeInfinity,
-                float.PositiveInfinity, ct).move;
-
-        private (FutureMove move, float value) NegaMax(Board board, PColor turn, int depth, float alpha, float beta, CancellationToken ct)
+        /// <summary>
+        /// The Setup() method, optional override
+        /// </summary>
+        /// <param name="str"> A string of the argument passed </param>
+        public override void Setup(string str)
         {
+            // Try to get the maximum depth from the parameters
+            if (!int.TryParse(str, out maxDepth))
+            {
+                // If not possible, set it to 3 by default
+                maxDepth = 4;
+            }
+        }
+
+        /// <summary>
+        /// The Think method (mandatory override) is invoked by the game engine
+        /// </summary>
+        /// <param name="board"> The current state of the board </param>
+        /// <param name="ct"> A cancelletion token </param>
+        /// <returns></returns>
+        public override FutureMove Think(Board board, CancellationToken ct)
+        {
+            // Invokes the NegaMax method returning a value and a furture move
+            return NegaMax(board.Copy(), board.Turn, 0, float.NegativeInfinity,
+                float.PositiveInfinity, ct).move;
+        }
+
+        /// <summary>
+        /// NegaMax method responsible for finding the best move
+        /// </summary>
+        /// <param name="board"> The current board </param>
+        /// <param name="turn"> Who's turn it is </param>
+        /// <param name="depth"> The current depth </param>
+        /// <param name="alpha"> Alpha value </param>
+        /// <param name="beta"> Beta value </param>
+        /// <param name="ct"> Cancellation token </param>
+        /// <returns> A future move and a value </returns>
+        private (FutureMove move, float value) NegaMax(Board board, PColor turn
+            , int depth, float alpha, float beta, CancellationToken ct)
+        {
+            // Checks if a cancelletion was called
             if (ct.IsCancellationRequested) return (FutureMove.NoMove, 0);
 
+            // Checks if there's a winner
             if (board.CheckWinner() != Winner.None)
             {
+                // If the winner is the current player
                 if (board.CheckWinner() == turn.ToWinner())
                 {
+                    // Returns the maximum socre
                     return (FutureMove.NoMove, winScore);
                 }
                 else
+                    // Returns the minimum score
                     return (FutureMove.NoMove, -winScore);
             }
+            // Checks if the current depth is the maximum
             else if (depth == maxDepth)
             {
+                // Finds the heuristic value of the board and returns it
                 return (FutureMove.NoMove, HeuristicValue(board, turn));
             }
+            // If none of the above
             else
             {
-                (FutureMove move, float value) bestMove = (FutureMove.NoMove, float.NegativeInfinity);
+                // Creates a variable with a future move and value
+                (FutureMove move, float value) bestMove =
+                    (FutureMove.NoMove, float.NegativeInfinity);
 
-                for (int i = 0; i < board.cols; i++)
+                // Runs a cycle for each collumn in the board
+                for (int colsN = 0; colsN < board.cols; colsN++)
                 {
-                    if (board.IsColumnFull(i))
-                        continue;
+                    // If the collumn is full it skips the rest 
+                    if (board.IsColumnFull(colsN)) continue;
 
-                    for (int b = 0; b < 2; b++)
+                    // A loop for checking both square and circle pieces
+                    for (int shapeN = 0; shapeN < 2; shapeN++)
                     {
-                        FutureMove pos = new FutureMove(i, (PShape)b);
-
-                        if (board.PieceCount(turn, pos.shape) == 0)
+                        // Checks if the current player has pieces of the type
+                        if (board.PieceCount(turn, (PShape)shapeN) == 0)
                             continue;
-                        //    pos = pos.shape == PShape.Square ? new FutureMove(i, PShape.Round) : new FutureMove(i, PShape.Square);
 
-                        //if (board.PieceCount(turn, PShape.Round) == 0 && board.PieceCount(turn, PShape.Square) == 0)
-                        //    return bestMove;
+                        // Creates a new FutureMove with the current collumn
+                        // and the PShape type acording to b
+                        FutureMove pos = new FutureMove(colsN, (PShape)shapeN);
 
+                        // Does a move on the board with the pos values
                         board.DoMove(pos.shape, pos.column);
 
+                        // Creates a score variable and assigns it the value
+                        // given by an iteration of the NegaMax method
                         float score = -NegaMax(board.Copy(), turn.Other(), depth + 1, -beta, -alpha, ct).value;
 
+                        // Undos the move done
                         board.UndoMove();
 
+                        // Checks if the score of a board is bigger than alpha
                         if (score > alpha)
                         {
+                            // Assigns the value of aplha the value of score
                             alpha = score;
+                            // Assigns the bestMove the value of pos and alpha
                             bestMove = (pos, alpha);
 
+                            // Checks if there's Alpha Beta cuts
                             if (alpha >= beta)
+                                // If there is returns the current bestMove
                                 return bestMove;
                         }
                     }
                 }
+                // If all else fails returns the current bestMove
                 return bestMove;
             }
         }
