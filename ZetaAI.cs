81ee6c92171162dee438251f9b329a407d53c564
Freeze88 <andrevitorino1997@hotmail.com>
2020-03-29 19:40:43 +0100

    Rework heuristic, move heuristic to seperate class

--- ZetaAI.cs
+++ ZetaAI.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace ColorShapeLinks.Common.AI.ZetaAI
 {
-    internal class ZetaAI : AbstractThinker
+    class ZetaAI : AbstractThinker
     {
         private readonly float winScore = 100000;
         private int maxDepth;
+        private IEnumerable<Pos>[] positions;
+        private bool firsttime;
+        private readonly ZetaHeuristic heuristic;
 
+        public ZetaAI()
+        {
+            heuristic = new ZetaHeuristic();
+            firsttime = true;
+        }
         /// <summary>
         /// The Setup() method, optional override
         /// </summary>
@@ -18,7 +27,7 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
             if (!int.TryParse(str, out maxDepth))
             {
                 // If not possible, set it to 3 by default
-                maxDepth = 4;
+                maxDepth = 3;
             }
         }
 
@@ -30,9 +39,16 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
         /// <returns></returns>
         public override FutureMove Think(Board board, CancellationToken ct)
         {
+            if (firsttime)
+            {
+                positions = board.winCorridors.ToArray();
+                firsttime = false;
+            }
             // Invokes the NegaMax method returning a value and a furture move
-            return NegaMax(board.Copy(), board.Turn, 0, float.NegativeInfinity,
-                float.PositiveInfinity, ct).move;
+            (FutureMove move, float value) b = NegaMax(board.Copy(), board.Turn, 0, float.NegativeInfinity,
+                float.PositiveInfinity, ct);
+
+            return b.move;
         }
 
         /// <summary>
@@ -49,7 +65,10 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
             , int depth, float alpha, float beta, CancellationToken ct)
         {
             // Checks if a cancelletion was called
-            if (ct.IsCancellationRequested) return (FutureMove.NoMove, 0);
+            if (ct.IsCancellationRequested)
+            {
+                return (FutureMove.NoMove, 0);
+            }
 
             // Checks if there's a winner
             if (board.CheckWinner() != Winner.None)
@@ -61,14 +80,16 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                     return (FutureMove.NoMove, winScore);
                 }
                 else
+                {
                     // Returns the minimum score
                     return (FutureMove.NoMove, -winScore);
+                }
             }
             // Checks if the current depth is the maximum
             else if (depth == maxDepth)
             {
                 // Finds the heuristic value of the board and returns it
-                return (FutureMove.NoMove, HeuristicValue(board, turn));
+                return (FutureMove.NoMove, heuristic.HeuristicValue(board, turn, positions));
             }
             // If none of the above
             else
@@ -81,14 +102,19 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                 for (int colsN = 0; colsN < board.cols; colsN++)
                 {
                     // If the collumn is full it skips the rest 
-                    if (board.IsColumnFull(colsN)) continue;
+                    if (board.IsColumnFull(colsN))
+                    {
+                        continue;
+                    }
 
                     // A loop for checking both square and circle pieces
                     for (int shapeN = 0; shapeN < 2; shapeN++)
                     {
                         // Checks if the current player has pieces of the type
                         if (board.PieceCount(turn, (PShape)shapeN) == 0)
+                        {
                             continue;
+                        }
 
                         // Creates a new FutureMove with the current collumn
                         // and the PShape type acording to b
@@ -99,7 +125,7 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
 
                         // Creates a score variable and assigns it the value
                         // given by an iteration of the NegaMax method
-                        float score = -NegaMax(board.Copy(), turn.Other(), depth + 1, -beta, -alpha, ct).value;
+                        float score = -NegaMax(board, turn.Other(), depth + 1, -beta, -alpha, ct).value;
 
                         // Undos the move done
                         board.UndoMove();
@@ -114,8 +140,10 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
 
                             // Checks if there's Alpha Beta cuts
                             if (alpha >= beta)
+                            {
                                 // If there is returns the current bestMove
                                 return bestMove;
+                            }
                         }
                     }
                 }
@@ -123,295 +151,10 @@ namespace ColorShapeLinks.Common.AI.ZetaAI
                 return bestMove;
             }
         }
-
-        private float HeuristicValue(Board board, PColor turn)
-        {
-            float boardValue = 0;
-            int nOfCollums = 0;
-
-            float centerRow = board.rows / 2;
-            float centerCol = board.cols / 2;
-
-            float maxPoints = Dist(centerRow, centerCol, 0, 0);
-
-            for (int j = 0; j < board.cols; j++)
-            {
-                for (int f = 0; f < board.rows; f++)
-                {
-                    if (board[f, j].HasValue)
-                    {
-                        Piece? piece = board[f, j];
-
-                        if (turn.FriendOf(piece.Value))
-                            boardValue += maxPoints - Dist(centerRow, centerCol, j, f);
-                        else
-                            boardValue -= maxPoints - Dist(centerRow, centerCol, j, f);
-
-                        // Both sides
-                        if (j - 1 >= 0 && j + 1 <= board.cols - 1)
-                        {
-                            Piece? emptySpace = piece;
-                            Piece? emptySpace2 = piece;
-
-                            if (j + 2 <= board.cols - 1)
-                            {
-                                emptySpace = board[f, j + 2];
-                            }
-                            if (j - 2 >= 0)
-                            {
-                                emptySpace2 = board[f, j - 2];
-                            }
-
-                            if (board[f, j - 1].HasValue && board[f, j + 1].HasValue && (!emptySpace.HasValue || !emptySpace2.HasValue))
-                            {
-                                if ((board[f, j - 1].Value.color.FriendOf(piece.Value) && board[f, j + 1].Value.color.FriendOf(piece.Value)))
-                                {
-                                    int boardExtra = 100;
-                                    int columsLess = 1;
-
-                                    if (!turn.FriendOf(piece.Value))
-                                    {
-                                        boardExtra = -boardExtra;
-                                        columsLess = -columsLess;
-                                    }
-
-                                    if (!emptySpace.HasValue && !emptySpace2.HasValue)
-                                    {
-                                        boardValue += boardExtra;
-                                        nOfCollums += columsLess;
-                                    }
-                                    else if (!emptySpace.HasValue || !emptySpace2.HasValue)
-                                    {
-                                        boardValue += boardExtra * 2;
-                                        nOfCollums += columsLess;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (board[f, j - 1].HasValue)
-                                {
-                                    if (board[f, j - 1].Value.color.FriendOf(piece.Value))
-                                    {
-                                        if (turn.FriendOf(piece.Value))
-                                            boardValue += 2;
-                                        else
-                                            boardValue -= 2;
-                                    }
-                                }
-                                if (board[f, j + 1].HasValue)
-                                {
-                                    if (board[f, j + 1].Value.color.FriendOf(piece.Value))
-                                    {
-                                        if (turn.FriendOf(piece.Value))
-                                            boardValue += 2;
-                                        else
-                                            boardValue -= 2;
-                                    }
-                                }
-                            }
-                        }
-
-                        // Diagonals right
-                        if (f - 2 >= 0 && j + 2 <= board.cols - 1)
-                        {
-                            Piece? emptySpace = piece;
-                            Piece? emptySpace2 = piece;
-
-                            if (f - 3 >= 0 && j + 3 <= board.cols - 1)
-                            {
-                                emptySpace = board[f - 3, j + 3];
-                            }
-                            if (f + 1 <= board.rows - 1 && j - 1 >= 0)
-                            {
-                                emptySpace2 = board[f + 1, j - 1];
-                            }
-
-                            if (board[f - 2, j + 2].HasValue && board[f - 1, j + 1].HasValue && (!emptySpace.HasValue || !emptySpace2.HasValue))
-                            {
-                                if ((board[f - 1, j + 1].Value.color.FriendOf(piece.Value) && board[f - 2, j + 2].Value.color.FriendOf(piece.Value)))
-                                {
-                                    if (piece.Value.shape == turn.Shape() || piece.Value.color == turn)
-                                    {
-                                        int boardExtra = 100;
-                                        int columsLess = 1;
-
-                                        if (!turn.FriendOf(piece.Value))
-                                        {
-                                            boardExtra = -boardExtra;
-                                            columsLess = -columsLess;
-                                        }
-
-                                        if (!emptySpace.HasValue && !emptySpace2.HasValue)
-                                        {
-                                            boardValue += boardExtra;
-                                            nOfCollums += columsLess;
-                                        }
-                                        else if (!emptySpace.HasValue || !emptySpace2.HasValue)
-                                        {
-                                            boardValue += boardExtra * 2;
-                                            nOfCollums += columsLess;
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (board[f - 1, j + 1].HasValue)
-                                {
-                                    if (board[f - 1, j + 1].Value.color.FriendOf(piece.Value))
-                                    {
-                                        if (turn.FriendOf(piece.Value))
-                                            boardValue += 2;
-                                        else
-                                            boardValue -= 2;
-                                    }
-                                }
-                                if (f + 1 <= board.rows - 1 && j - 1 >= 0)
-                                {
-                                    if (board[f + 1, j - 1].HasValue)
-                                    {
-                                        if (board[f + 1, j - 1].Value.color.FriendOf(piece.Value))
-                                        {
-                                            if (turn.FriendOf(piece.Value))
-                                                boardValue += 2;
-                                            else
-                                                boardValue -= 2;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        // Diagonals left
-                        if (f - 2 >= 0 && j - 1 >= 0)
-                        {
-                            Piece? emptySpace = piece;
-                            Piece? emptySpace2 = piece;
-
-                            if (f - 3 >= 0 && j - 3 >= 0)
-                            {
-                                emptySpace = board[f - 3, j - 3];
-                            }
-                            if (f + 1 <= board.rows - 1 && j + 1 <= board.cols - 1)
-                            {
-                                emptySpace2 = board[f + 1, j + 1];
-                            }
-
-                            if (f - 2 >= 0 && j - 2 >= 0)
-                            {
-                                if (board[f - 2, j - 2].HasValue && board[f - 1, j - 1].HasValue && (!emptySpace.HasValue || !emptySpace2.HasValue))
-                                {
-                                    if ((board[f - 1, j - 1].Value.color.FriendOf(piece.Value) && board[f - 2, j - 2].Value.color.FriendOf(piece.Value)))
-                                    {
-                                        int boardExtra = 100;
-                                        int columsLess = 1;
-
-                                        if (!turn.FriendOf(piece.Value))
-                                        {
-                                            boardExtra = -boardExtra;
-                                            columsLess = -columsLess;
-                                        }
-
-                                        if (!emptySpace.HasValue && !emptySpace2.HasValue)
-                                        {
-                                            boardValue += boardExtra;
-                                            nOfCollums += columsLess;
-                                        }
-                                        else if (!emptySpace.HasValue || !emptySpace2.HasValue)
-                                        {
-                                            boardValue += boardExtra * 2;
-                                            nOfCollums += columsLess;
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (board[f - 1, j - 1].HasValue)
-                                {
-                                    if (board[f - 1, j - 1].Value.color.FriendOf(piece.Value))
-                                    {
-                                        if (turn.FriendOf(piece.Value))
-                                            boardValue += 2;
-                                        else
-                                            boardValue -= 2;
-                                    }
-                                }
-                                if (f + 1 <= board.rows - 1 && j + 1 <= board.cols - 1)
-                                {
-                                    if (board[f + 1, j + 1].HasValue)
-                                    {
-                                        if (board[f + 1, j + 1].Value.color.FriendOf(piece.Value))
-                                        {
-                                            if (turn.FriendOf(piece.Value))
-                                                boardValue += 2;
-                                            else
-                                                boardValue -= 2;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        // Down
-                        if (f - 1 >= 0 && f - 2 >= 0)
-                        {
-                            Piece? emptySpace = piece;
-
-                            if (f + 1 < board.rows - 1)
-                            {
-                                emptySpace = board[f + 1, j];
-                            }
-
-                            if (board[f - 1, j].HasValue && board[f - 2, j].HasValue && f + 1 <= board.rows - 1)
-                            {
-                                if ((board[f - 1, j].Value.color.FriendOf(piece.Value) && board[f - 2, j].Value.color.FriendOf(piece.Value)))
-                                {
-                                    int boardExtra = 100;
-                                    int columsLess = 1;
-
-                                    if (!turn.FriendOf(piece.Value))
-                                    {
-                                        boardExtra = -boardExtra;
-                                        columsLess = -columsLess;
-                                    }
-
-                                    if (!emptySpace.HasValue)
-                                    {
-                                        boardValue += boardExtra;
-                                        nOfCollums += columsLess;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (board[f - 1, j].HasValue)
-                                {
-                                    if (board[f - 1, j].Value.color.FriendOf(piece.Value))
-                                    {
-                                        if (turn.FriendOf(piece.Value))
-                                            boardValue += 2;
-                                        else
-                                            boardValue -= 2;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if (nOfCollums > 1)
-                boardValue += 1000;
-            else if (nOfCollums < -1)
-                boardValue -= 1000;
-            return (boardValue * 100);
-        }
-
-        private float Dist(float x1, float y1, float x2, float y2)
-        {
-            return (float)Math.Sqrt(
-                Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-        }
     }
 }
+
+
+
+
+
---
+++ ZetaHeuristic.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ColorShapeLinks.Common;
+
+public class ZetaHeuristic
+{
+    private int inRowFriend;
+    private int inRowEnemy;
+    private int emptySpcaces;
+    private bool thisBegin;
+    private bool thisEnd;
+    private bool otherBegin;
+    private bool otherEnd;
+    private bool thisHasPieces;
+    private bool otherHasPieces;
+
+    public float HeuristicValue(Board board, PColor turn, IEnumerable<Pos>[] positions)
+    {
+        float boardValue = 0;
+        float centerRow = board.rows / 2;
+        float centerCol = board.cols / 2;
+        float maxPoints = Dist(centerRow, centerCol, 0, 0);
+
+        inRowFriend = 0;
+        inRowEnemy = 0;
+        emptySpcaces = 0;
+
+        thisBegin = false;
+        thisEnd = false;
+        otherBegin = false;
+        otherEnd = false;
+        thisHasPieces = false;
+        otherHasPieces = false;
+
+        foreach (IEnumerable rows in positions)
+        {
+            foreach (Pos pos in rows)
+            {
+                if (board[pos.row, pos.col].HasValue)
+                {
+                    if (turn.FriendOf(board[pos.row, pos.col].Value))
+                    {
+                        boardValue += maxPoints - Dist(centerRow, centerCol,
+                            pos.col, pos.row);
+                    }
+                    else
+                    {
+                        boardValue -= maxPoints - Dist(centerRow, centerCol,
+                            pos.col, pos.row);
+                    }
+                }
+                boardValue = EvaluateShapeAndColor(board, pos, turn,
+                    boardValue, true);
+                boardValue = EvaluateShapeAndColor(board, pos, turn,
+                    boardValue, false);
+            }
+            boardValue += inRowFriend * inRowFriend - inRowEnemy * inRowEnemy;
+        }
+        return (boardValue);
+    }
+    private float EvaluateShapeAndColor(Board board, Pos pos, PColor turn,
+        float boardValue, bool color)
+    {
+        int inSequence = board.piecesInSequence - 1;
+
+        if (board[pos.row, pos.col].HasValue)
+        {
+            if (color ? board[pos.row, pos.col].Value.color == turn :
+                board[pos.row, pos.col].Value.shape == turn.Shape())
+            {
+                inRowFriend++;
+                inRowEnemy = 0;
+            }
+            else
+            {
+                inRowEnemy++;
+                inRowFriend = 0;
+            }
+        }
+        else
+        {
+            if (!thisHasPieces)
+            {
+                thisBegin = true;
+            }
+            if (inRowFriend == inSequence)
+            {
+                thisEnd = true;
+            }
+
+            if (!otherHasPieces)
+            {
+                otherBegin = true;
+            }
+
+            if (inRowEnemy == inSequence)
+            {
+                otherEnd = true;
+            }
+            emptySpcaces++;
+        }
+
+        if (emptySpcaces >= 2)
+        {
+            boardValue += inRowFriend * inRowFriend - inRowEnemy * inRowEnemy;
+
+            if (inRowFriend == inSequence)
+            {
+                inRowFriend = 0;
+
+                if (thisEnd || thisBegin)
+                {
+                    boardValue += 50;
+                }
+                if (thisEnd && thisBegin)
+                {
+                    boardValue += 1000;
+                }
+            }
+            if (inRowEnemy == inSequence)
+            {
+                inRowEnemy = 0;
+
+                if (otherEnd || otherBegin)
+                {
+                    boardValue -= 50;
+                }
+                if (otherEnd && otherBegin)
+                {
+                    boardValue -= 1000;
+                }
+            }
+        }
+        return boardValue;
+    }
+    private float Dist(float x1, float y1, float x2, float y2) =>
+        (float)Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+}
+
